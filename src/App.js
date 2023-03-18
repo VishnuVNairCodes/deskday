6e3d6c31e4ccc687361d0e4a1f3cd27d1f9f623b
Vishnu V Nair <vishnuvnair.ui@gmail.com>
2023-03-19 01:23:17 +0530

    style: create layout of app using grid

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import "./App.css";
 function App() {
   return (
     <div className="app">
-      <h1>App</h1>
+      <div className="sidebar">sidebar</div>
+      <div className="header">header</div>
+      <div className="main">body</div>
     </div>
   );
 }
