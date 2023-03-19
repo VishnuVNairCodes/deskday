04a86d8df2f3633d8bd919aa702a6eb422d2e8b2
Vishnu V Nair <vishnuvnair.ui@gmail.com>
2023-03-20 02:39:16 +0530

    feat: add AsideLogout component

--- src/components/SideBar/components/AsideLogout/index.jsx
+++ src/components/SideBar/components/AsideLogout/index.jsx
@@ -1,7 +1,22 @@
+import { useAuth } from "../../../../contexts/auth-context";
+import { Avatar } from "../../../Avatar";
 import "./AsideLogout.css";
 
 const AsideLogout = () => {
-  return <div className="aside-logout-container">Logout</div>;
+  const {
+    employeeDetails: { firstName, lastName },
+  } = useAuth();
+  return (
+    <div className="aside-logout-container">
+      <Avatar size="small" />
+      <h1 className="aside-logout-name">
+        {firstName} {lastName}
+      </h1>
+      <button className="btn-icon aside-logout-btn-icon">
+        <i className="fa-solid fa-ellipsis-vertical"></i>
+      </button>
+    </div>
+  );
 };
 
 export { AsideLogout };
