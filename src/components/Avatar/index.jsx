65bd8d865e5b039e96c06260a02ae6a16bec97b6
Vishnu V Nair <vishnuvnair.ui@gmail.com>
2023-03-20 00:49:58 +0530

    feat: add profileImage to Avatar

--- src/components/Avatar/index.jsx
+++ src/components/Avatar/index.jsx
@@ -1,7 +1,13 @@
+import { useAuth } from "../../contexts/auth-context";
 import "./Avatar.css";
 
 const Avatar = ({ size }) => {
-  return <img className={`avatar avatar-${size}`} alt="avatar" />;
+  const {
+    employeeDetails: { profileImage },
+  } = useAuth();
+  return (
+    <img className={`avatar avatar-${size}`} src={profileImage} alt="avatar" />
+  );
 };
 
 export { Avatar };
