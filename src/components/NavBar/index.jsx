62bfaa5c30b8d8ba1a2fea85be77a5937d3d814f
Vishnu V Nair <vishnuvnair.ui@gmail.com>
2023-03-21 07:39:32 +0530

    style: update gradient colors

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -6,7 +6,9 @@ const NavBar = () => {
     color: isActive ? "" : "var(--clr-nav-link-inactive)",
     fontWeight: isActive ? "var(--fwt-semi-bold)" : "var(--fwt-regular)",
     borderBottom: isActive ? "4px solid transparent" : "none",
-    background: isActive ? "var(--clr-gradient-text)" : "",
+    background: isActive
+      ? "radial-gradient(94.76% 4003.41% at 5.24% 29.55%, #4652CD 0%, #46CBEA 100%)"
+      : "",
     backgroundClip: isActive ? "text" : "",
     WebkitBackgroundClip: isActive ? "text" : "",
     WebkitTextFillColor: isActive ? "transparent" : "",
